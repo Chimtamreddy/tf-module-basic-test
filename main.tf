7e21282d29b18bf1d59ed1d6b134bf04ede800f3
Chimtamreddy <kr7348202@gmail.com>
2024-04-17 18:33:07 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "null_resource" "ansible" {
   ]
   provisioner "local-exec" {
     command = <<EOF
-cd /home/centos/roboshop-ansi
+cd /home/centos/roboshop-ansib
 git pull
 sleep 30
 ansible-playbook -i ${var.name}-dev.kr7348202.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
